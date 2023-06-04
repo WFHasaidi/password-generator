555fce792edcd434e4e26cdcf294c0e4fe7ab4d0
SAIDI Amine <amine.saidi.ing@gmail.com>
2023-06-04 13:01:11 +0200

    Add password generator route

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,6 +11,13 @@ class PagesController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function home(): Response
     {
+        //dd("Test");
         return $this->render('pages/home.html.twig');
+    }
+
+    #[Route('/password-generator', name: 'app_password_generator')]
+    public function password_generator(): Response
+    {
+        return $this->render('pages/password-generator.html.twig');
     }
 }
---
+++ src/Service/PasswordGenerator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Service;
+
+class PasswordGenerator
+{
+    public function generate()
+    {
+
+
+    }
+
+}
